5d2563cb9a7f72543a2241f57de7f0073915b31c
Marcus Zetterquist <marcus.zetterquist@gmail.com>
2016-08-12 02:46:13 +0200

    More tests for json stuff

--- FloydSpeak/FloydSpeak/json_support.cpp
+++ FloydSpeak/FloydSpeak/json_support.cpp
@@ -151,6 +151,8 @@ bool json_value_t::operator==(const json_value_t& other) const{
 	return _type == other._type && _object == other._object && _array == other._array && _string == other._string && _number == other._number;
 }
 
+
+
 QUARK_UNIT_TESTQ("json_value_t()", ""){
 	std::map<string, json_value_t> value {
 		{ "one", json_value_t(1.0) },
@@ -168,6 +170,60 @@ QUARK_UNIT_TESTQ("json_value_t()", ""){
 	QUARK_UT_VERIFY((a.get_object() != std::map<string, json_value_t>()));
 }
 
+QUARK_UNIT_TESTQ("json_value_t()", ""){
+	std::map<string, json_value_t> value {
+		{ "one", json_value_t(1.0) },
+		{ "two", json_value_t("zwei") },
+		{
+			"three",
+			json_value_t(
+				std::map<string, json_value_t>{
+					{ "A", json_value_t("aaa") },
+					{ "B", json_value_t("bbb") },
+					{ "C", json_value_t("ccc") }
+				}
+			)
+		}
+	};
+	const auto a = json_value_t(value);
+	QUARK_UT_VERIFY(a.is_object());
+	QUARK_UT_VERIFY(!a.is_array());
+	QUARK_UT_VERIFY(!a.is_string());
+	QUARK_UT_VERIFY(!a.is_number());
+	QUARK_UT_VERIFY(!a.is_true());
+	QUARK_UT_VERIFY(!a.is_false());
+	QUARK_UT_VERIFY(!a.is_null());
+	QUARK_UT_VERIFY(a.get_object() == value);
+	QUARK_UT_VERIFY(a.get_object().at("one") == json_value_t(1.0));
+	QUARK_UT_VERIFY(a.get_object().at("three").get_object().at("C") == json_value_t("ccc"));
+}
+
+
+QUARK_UNIT_TESTQ("json_value_t()", ""){
+	std::vector<json_value_t> value { json_value_t("A"), json_value_t("B") };
+	const auto a = json_value_t(value);
+	QUARK_UT_VERIFY(!a.is_object());
+	QUARK_UT_VERIFY(a.is_array());
+	QUARK_UT_VERIFY(!a.is_string());
+	QUARK_UT_VERIFY(!a.is_number());
+	QUARK_UT_VERIFY(!a.is_true());
+	QUARK_UT_VERIFY(!a.is_false());
+	QUARK_UT_VERIFY(!a.is_null());
+	QUARK_UT_VERIFY(a.get_array() == value);
+//	QUARK_UT_VERIFY((a.get_object() != std::map<string, json_value_t>()));
+}
+
+QUARK_UNIT_TESTQ("json_value_t()", ""){
+	const auto a = json_value_t("hello");
+	QUARK_UT_VERIFY(!a.is_object());
+	QUARK_UT_VERIFY(!a.is_array());
+	QUARK_UT_VERIFY(a.is_string());
+	QUARK_UT_VERIFY(!a.is_number());
+	QUARK_UT_VERIFY(!a.is_true());
+	QUARK_UT_VERIFY(!a.is_false());
+	QUARK_UT_VERIFY(!a.is_null());
+	QUARK_UT_VERIFY(a.get_string() == "hello");
+}
 
 QUARK_UNIT_TESTQ("json_value_t()", ""){
 	const auto a = json_value_t(123.0);
@@ -182,15 +238,37 @@ QUARK_UNIT_TESTQ("json_value_t()", ""){
 }
 
 QUARK_UNIT_TESTQ("json_value_t()", ""){
-	const auto a = json_value_t("hello");
+	const auto a = json_value_t(true);
 	QUARK_UT_VERIFY(!a.is_object());
 	QUARK_UT_VERIFY(!a.is_array());
-	QUARK_UT_VERIFY(a.is_string());
+	QUARK_UT_VERIFY(!a.is_string());
+	QUARK_UT_VERIFY(!a.is_number());
+	QUARK_UT_VERIFY(a.is_true());
+	QUARK_UT_VERIFY(!a.is_false());
+	QUARK_UT_VERIFY(!a.is_null());
+}
+
+QUARK_UNIT_TESTQ("json_value_t()", ""){
+	const auto a = json_value_t(false);
+	QUARK_UT_VERIFY(!a.is_object());
+	QUARK_UT_VERIFY(!a.is_array());
+	QUARK_UT_VERIFY(!a.is_string());
+	QUARK_UT_VERIFY(!a.is_number());
+	QUARK_UT_VERIFY(!a.is_true());
+	QUARK_UT_VERIFY(a.is_false());
+	QUARK_UT_VERIFY(!a.is_null());
+}
+
+QUARK_UNIT_TESTQ("json_value_t()", ""){
+	const auto a = json_value_t();
+	QUARK_UT_VERIFY(!a.is_object());
+	QUARK_UT_VERIFY(!a.is_array());
+	QUARK_UT_VERIFY(!a.is_string());
 	QUARK_UT_VERIFY(!a.is_number());
 	QUARK_UT_VERIFY(!a.is_true());
 	QUARK_UT_VERIFY(!a.is_false());
-	QUARK_UT_VERIFY(!a.is_null());
-	QUARK_UT_VERIFY(a.get_string() == "hello");
+	QUARK_UT_VERIFY(a.is_null());
 }
+
 
 
