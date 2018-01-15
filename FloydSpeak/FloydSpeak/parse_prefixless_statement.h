f4894c50fd8af9922984a3a07f3fa069781a8a58
marcusz <marcusz@users.noreply.github.com>
2018-01-15 15:14:21 +0100

    Moved all related prefixless statement parsing into new file.

--- FloydSpeak/FloydSpeak/parse_prefixless_statement.h
+++ FloydSpeak/FloydSpeak/parse_prefixless_statement.h
@@ -18,6 +18,41 @@ namespace floyd {
 
 	std::pair<json_t, seq_t> parse_prefixless_statement(const seq_t& s);
 
+
+	/*
+		int a = 10;
+		float b = 0.3;
+		int c = a + b;
+		int b = f(a);
+		string hello = f(a) + "_suffix";;
+
+		mutable a = 10;
+
+		...can contain trailing whitespace.
+
+
+		int b = f("hello");
+		bool a = is_hello("hello");
+
+		OUTPUT:
+			[ "bind", "float", "x", EXPRESSION ]
+	*/
+	std::pair<json_t, seq_t> parse_bind_statement(const seq_t& s);
+
+	/*
+		x = expression:
+		mutable x = expression;
+
+		x = 4
+	*/
+	std::pair<json_t, seq_t> parse_assign_statement(const seq_t& s);
+
+
+	/*
+		print(13);
+	*/
+	std::pair<json_t, seq_t> parse_expression_statement(const seq_t& s);
+
 }
 
 #endif /* parse_implicit_statement_hpp */
