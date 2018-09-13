c6dd42211dd9e3530eac4a153b9751e9da65d183
Marcus Zetterquist <marcus.zetterquist@gmail.com>
2018-09-13 15:11:28 +0200

    Added C++11 threading bits

--- dev/floyd_speak/floyd_main.cpp
+++ dev/floyd_speak/floyd_main.cpp
@@ -24,6 +24,162 @@
 #include "interpretator_benchmark.h"
 
 
+
+
+
+
+
+#include <cmath>
+#include <thread>
+#include <future>
+
+
+/////////////////        Multi-threading example
+
+
+
+struct pixel {
+	pixel(float red, float green, float blue, float alpha) :
+	_red(red),
+	_green(green),
+	_blue(blue),
+	_alpha(alpha)
+	{
+	}
+	pixel() = default;
+	pixel& operator=(const pixel& rhs) = default;
+	pixel(const pixel& rhs) = default;
+	
+	
+	////////////////    State
+	
+	float _red = 0.0f;
+	float _green = 0.0f;
+	float _blue = 0.0f;
+	float _alpha = 0.0f;
+};
+
+struct image {
+	int _width;
+	int _height;
+	immer::vector<pixel> _pixels;
+};
+
+image make_image(int width, int height){
+	immer::vector<pixel> pixels;
+	
+	for(auto y = 0 ; y < height ; y++){
+		for(auto x = 0 ; x < width ; x++){
+			float s = std::sin(static_cast<float>(M_PI) * 2.0f * (float)x);
+			pixel p(0.5f + s, 0.5f, 0.0f, 1.0f);
+			pixels.push_back(p);
+		}
+	}
+	
+	image result;
+	result._width = width;
+	result._height = height;
+	result._pixels = pixels;
+	return result;
+}
+
+
+//    There is no way to trip-up caller because image is a copy.
+image worker8(image img) {
+	const size_t count = std::min<size_t>(300, img._pixels.size());
+	for(size_t i = 0 ; i < count ; i++){
+		auto pixel = img._pixels[i];
+		pixel._red = 1.0f - pixel._red;
+		img._pixels = img._pixels.set(i, pixel);
+	}
+	return img;
+}
+
+//    Demonstates it is safe and easy and efficent to use immer::vector when sharing data between threads.
+void example8(){
+	QUARK_SCOPED_TRACE(__FUNCTION__);
+	
+	const auto a = make_image(700, 700);
+	
+//    const auto inodes1 = steady::get_inode_count<pixel>();
+//    const auto leaf_nodes1 = steady::get_leaf_count<pixel>();
+//    QUARK_TRACE_SS("When a exists: inodes: " << inodes1 << ", leaf nodes: " << leaf_nodes1);
+	
+	//    Get a workerthread process image a in the background.
+	//    We give the worker a copy of a. This is free.
+	//    There is no way for worker thread to have side effects on image a.
+	auto future = std::async(worker8, a);
+	
+	//    ...meanwhile, do some processing of our own on mage a!
+	auto b = a;
+	for(int i = 0 ; i < 200 ; i++){
+		auto pixel = b._pixels[i];
+		pixel._red = 1.0f - pixel._red;
+		b._pixels = b._pixels.set(i, pixel);
+	}
+	
+	//    Block on worker finishing.
+	image c = future.get();
+	
+	
+//    const auto inodes2 = steady::get_inode_count<pixel>();
+//    const auto leaf_nodes2 = steady::get_leaf_count<pixel>();
+	//QUARK_TRACE_SS("When b + c exists: inodes: " << inodes2 << ", leaf nodes: " << leaf_nodes2);
+	
+//    const auto extra_inodes = inodes2 - inodes1;
+//    const auto extra_leaf_nodes = leaf_nodes2 - leaf_nodes1;
+	
+//    QUARK_TRACE_SS("Extra storage requried for a + b: inodes: " << extra_inodes << ", leaf nodes: " << extra_leaf_nodes);
+}
+
+
+
+static const int k_hardware_thread_count = 8;
+
+
+
+
+void call_from_thread(int tid) {
+	std::cout << tid << std::endl;
+}
+
+struct runtime_t {
+	std::vector<std::thread> _worker_threads;
+
+
+	runtime_t(){
+		//	Remember that current thread (main) is also a thread.
+		for(int i = 0 ; i < k_hardware_thread_count - 1 ; i++){
+			_worker_threads.push_back(std::thread(call_from_thread, i));
+		}
+	}
+	~runtime_t(){
+		for(auto &t: _worker_threads){
+			t.join();
+		}
+	}
+};
+
+
+
+
+QUARK_UNIT_TEST("","", "", ""){
+	std::cout << "A" << std::endl;
+	{
+		runtime_t test_runtime;
+		std::cout << "B" << std::endl;
+	}
+	std::cout << "C" << std::endl;
+}
+
+
+
+
+
+
+
+
+
 //////////////////////////////////////////////////		main()
 
 #if true
@@ -133,13 +289,13 @@ void init_terminal(){
 #include<ncurses.h>
 
 void init_terminal(){
-             initscr(); cbreak(); noecho();
+			 initscr(); cbreak(); noecho();
 
 //       Most programs would additionally use the sequence:
 
-             nonl();
-             intrflush(stdscr, FALSE);
-             keypad(stdscr, TRUE);
+			 nonl();
+			 intrflush(stdscr, FALSE);
+			 keypad(stdscr, TRUE);
 }
 
 std::string get_command(){
@@ -469,3 +625,4 @@ int main(int argc, const char * argv[]) {
 
 	return 0;
 }
+
