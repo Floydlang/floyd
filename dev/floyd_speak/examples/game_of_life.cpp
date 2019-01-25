8d559d9e622f6a44be8c9dbaaceda1307f04d769
Marcus Zetterquist <marcus.zetterquist@gmail.com>
2019-01-25 12:40:21 +0100

    Cleaned up game-of-life C++ and Floyd version so you can compare them

--- dev/floyd_speak/examples/game_of_life.cpp
+++ dev/floyd_speak/examples/game_of_life.cpp
@@ -1,35 +1,16 @@
 //
-//  parser_evaluator.h
 //  FloydSpeak
 //
 //  Created by Marcus Zetterquist on 26/07/16.
 //  Copyright © 2016 Marcus Zetterquist. All rights reserved.
 //
 
-#include "interpretator_benchmark.h"
-
 #include "benchmark_basics.h"
 
-#include <string>
-#include <vector>
 #include <iostream>
 
-using std::vector;
-using std::string;
 
-using namespace floyd;
-
-
-
-//////////////////////////////////////////		TESTS
-
-
-const int64_t cpp_iterations = (50LL * 1000LL * 1000LL);
-const int64_t floyd_iterations = (50LL * 1000LL * 1000LL);
-
-
-
-//////////////////////////////////////////		GAME OF LIFE C
+//////////////////////////////////////////		GAME OF LIFE - C
 
 //	https://www.cs.utexas.edu/users/djimenez/utsa/cs1713-3/c/life.txt
 
@@ -46,12 +27,23 @@ const int64_t floyd_iterations = (50LL * 1000LL * 1000LL);
  * The game plays 100 rounds, printing to the screen each time.  'x' printed
  * means on, space means 0.
  *
- */
-//#include <stdio.h>
+	(copied this from some web page, hence the English spellings...)
+
+	1.STASIS : If, for a given cell, the number of on neighbours is
+	exactly two, the cell maintains its status quo into the next
+	generation. If the cell is on, it stays on, if it is off, it stays off.
+
+	2.GROWTH : If the number of on neighbours is exactly three, the cell
+	will be on in the next generation. This is regardless of the cell's
+	current state.
+
+	3.DEATH : If the number of on neighbours is 0, 1, 4-8, the cell will
+	be off in the next generation.
+*/
 
 /* dimensions of the screen */
 
-#define BOARD_WIDTH	79
+#define BOARD_WIDTH		79
 #define BOARD_HEIGHT	24
 
 /* set everthing to zero */
@@ -100,19 +92,6 @@ int adjacent_to (int board[][BOARD_HEIGHT], int i, int j) {
 }
 
 void play (int board[][BOARD_HEIGHT]) {
-/*
-	(copied this from some web page, hence the English spellings...)
-
-	1.STASIS : If, for a given cell, the number of on neighbours is
-	exactly two, the cell maintains its status quo into the next
-	generation. If the cell is on, it stays on, if it is off, it stays off.
-
-	2.GROWTH : If the number of on neighbours is exactly three, the cell
-	will be on in the next generation. This is regardless of the cell's 		current state.
-
-	3.DEATH : If the number of on neighbours is 0, 1, 4-8, the cell will
-	be off in the next generation.
-*/
 	int	i, j, a, newboard[BOARD_WIDTH][BOARD_HEIGHT];
 
 	/* for each cell, apply the rules of Life */
@@ -153,29 +132,6 @@ void print (int board[][BOARD_HEIGHT]) {
 	}
 }
 
-/* read a file into the life board */
-
-void read_file (int board[][BOARD_HEIGHT], char *name) {
-	FILE	*f;
-	int	i, j;
-	char	s[100];
-
-	f = fopen (name, "r");
-	for (j=0; j<BOARD_HEIGHT; j++) {
-
-		/* get a string */
-
-		fgets (s, 100, f);
-
-		/* copy the string to the life board */
-
-		for (i=0; i<BOARD_WIDTH; i++) {
-			board[i][j] = s[i] == 'x';
-		}
-	}
-	fclose (f);
-}
-
 /* main program */
 
 int test_main (int argc, char *argv[]) {
@@ -184,19 +140,20 @@ int test_main (int argc, char *argv[]) {
 	initialize_board (board);
 //	read_file (board, argv[1]);
 
-			board[BOARD_WIDTH / 2][BOARD_HEIGHT / 2] = 'x';
-			board[1 + BOARD_WIDTH / 2][1+ BOARD_HEIGHT / 2] = 'x';
-			board[1 + BOARD_WIDTH / 2][BOARD_HEIGHT / 2] = 'x';
-			board[2 + BOARD_WIDTH / 2][BOARD_HEIGHT / 2] = 'x';
-			board[2 + BOARD_WIDTH / 2][1 + BOARD_HEIGHT / 2] = 'x';
-			board[3 + BOARD_WIDTH / 2][BOARD_HEIGHT / 2] = 'x';
-			board[3 + BOARD_WIDTH / 2][1 + BOARD_HEIGHT / 2] = 'x';
+	board[BOARD_WIDTH / 2][BOARD_HEIGHT / 2] = 'x';
+	board[1 + BOARD_WIDTH / 2][1+ BOARD_HEIGHT / 2] = 'x';
+	board[1 + BOARD_WIDTH / 2][BOARD_HEIGHT / 2] = 'x';
+	board[2 + BOARD_WIDTH / 2][BOARD_HEIGHT / 2] = 'x';
+	board[2 + BOARD_WIDTH / 2][1 + BOARD_HEIGHT / 2] = 'x';
+	board[3 + BOARD_WIDTH / 2][BOARD_HEIGHT / 2] = 'x';
+	board[3 + BOARD_WIDTH / 2][1 + BOARD_HEIGHT / 2] = 'x';
 
 
 	/* play game of life 100 times */
 
-	for (i=0; i<1000; i++) {
-//		print (board);
+	for (i=0; i<100; i++) {
+		printf ("%d\n", i);
+		print (board);
 		play (board);
 
 		/* clear the screen using VT100 escape codes */
@@ -205,45 +162,7 @@ int test_main (int argc, char *argv[]) {
 	return 0;
 }
 
-
-
-
-//////////////////////////////////////////		GAME OF LIFE -- ported to Floyd -- direct-port
-
-
-const std::string gol_floyd_str = R"(
-	func int f(){
-		mutable int count = 0;
-		for (index in 1...5000) {
-			count = count + 1;
-		}
-		return 13;
-	}
-)";
-
-
 QUARK_UNIT_TEST_VIP("Basic performance", "Game of life", "", ""){
-	const auto cpp_ns = measure_execution_time_ns(
-		[&] {
-			test_main(0, nullptr);
-		}
-	);
-
-	const auto ast = compile_to_bytecode(gol_floyd_str);
-	interpreter_t vm(ast);
-	const auto f = find_global_symbol2(vm, "f");
-	QUARK_ASSERT(f != nullptr);
-
-	const auto floyd_ns = measure_execution_time_ns(
-		[&] {
-			const auto result = call_function(vm, bc_to_value(f->_value), {});
-		}
-	);
-
-	double cpp_iteration_time = (double)cpp_ns / (double)cpp_iterations;
-	double floyd_iteration_time = (double)floyd_ns / (double)floyd_iterations;
-	double k = cpp_iteration_time / floyd_iteration_time;
-	std::cout << "Floyd performance: " << k << std::endl;
+	test_main(0, nullptr);
 }
-
 
