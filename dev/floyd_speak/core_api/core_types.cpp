c8a825cd7e33777f19c45b8c2419ad0140185bf9
Marcus Zetterquist <marcus.zetterquist@gmail.com>
2019-02-06 21:59:13 +0100

    Removed unused files

--- dev/floyd_speak/core_api/core_types.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  core_types.cpp
-//  floyd_speak
-//
-//  Created by Marcus Zetterquist on 2019-01-05.
-//  Copyright © 2019 Marcus Zetterquist. All rights reserved.
-//
-
-#include "core_types.h"
