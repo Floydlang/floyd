6cd53d9d72ea33f462e0133ee9710e2b1cf53264
Marcus Zetterquist <marcus.zetterquist@gmail.com>
2019-08-29 13:04:45 +0200

    Fix naming

--- dev/floyd/language_validations/floyd_test_suite.cpp
+++ dev/floyd/language_validations/floyd_test_suite.cpp
@@ -60,14 +60,14 @@ unsupported syntax
 
 
 #define FLOYD_LANG_PROOF(class_under_test, function_under_test, scenario, expected_result) \
-	static void QUARK_UNIQUE_LABEL(cppext_unit_test_)(); \
-	static ::quark::unit_test_rec QUARK_UNIQUE_LABEL(rec)(__FILE__, __LINE__, class_under_test, function_under_test, scenario, expected_result, QUARK_UNIQUE_LABEL(cppext_unit_test_), false); \
-	static void QUARK_UNIQUE_LABEL(cppext_unit_test_)()
+	static void QUARK_UNIQUE_LABEL(quark_unit_test_)(); \
+	static ::quark::unit_test_rec QUARK_UNIQUE_LABEL(rec)(__FILE__, __LINE__, class_under_test, function_under_test, scenario, expected_result, QUARK_UNIQUE_LABEL(quark_unit_test_), false); \
+	static void QUARK_UNIQUE_LABEL(quark_unit_test_)()
 
 #define FLOYD_LANG_PROOF_VIP(class_under_test, function_under_test, scenario, expected_result) \
-	static void QUARK_UNIQUE_LABEL(cppext_unit_test_)(); \
-	static ::quark::unit_test_rec QUARK_UNIQUE_LABEL(rec)(__FILE__, __LINE__, class_under_test, function_under_test, scenario, expected_result, QUARK_UNIQUE_LABEL(cppext_unit_test_), true); \
-	static void QUARK_UNIQUE_LABEL(cppext_unit_test_)()
+	static void QUARK_UNIQUE_LABEL(quark_unit_test_)(); \
+	static ::quark::unit_test_rec QUARK_UNIQUE_LABEL(rec)(__FILE__, __LINE__, class_under_test, function_under_test, scenario, expected_result, QUARK_UNIQUE_LABEL(quark_unit_test_), true); \
+	static void QUARK_UNIQUE_LABEL(quark_unit_test_)()
 
 
 
